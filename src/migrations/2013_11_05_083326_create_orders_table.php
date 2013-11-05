8a475c0abbc02ba3a34a483bf0b773b11ccaf595
Boris Strahija <boris@creolab.hr>
2013-11-05 09:36:43 +0100

    Add orders migration

---
+++ src/migrations/2013_11_05_083326_create_orders_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('orders', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->integer('cart_id')->index();
+			$table->integer('count');
+			$table->float('total');
+			$table->string('status', 20);
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('orders');
+	}
+
+}
